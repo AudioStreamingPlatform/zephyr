b6019bf399dc684c2c4785752b737d1fd565d376
Emil Dahl Juhl <emdj@bang-olufsen.dk>
2024-08-22 13:27:34 +0200

    drivers: gpio: mspm0g3xxx: mask interrupts correctly
    Reading back the raw interrupt status from the gpio controller, provides
    the interrupt mask _without_ the controller's masking registers applied.
    This means that e.g. a rising edge would trigger an interrupt even on pins
    that are configured for falling edge only.
    
    Fix this by reading the "enabled" interrupt status instead of the raw one.
    
    Signed-off-by: Emil Dahl Juhl <emdj@bang-olufsen.dk>

--- drivers/gpio/gpio_mspm0.c
+++ drivers/gpio/gpio_mspm0.c
@@ -292,7 +292,7 @@ static void gpio_mspm0_isr(const struct device *port)
 	struct gpio_mspm0_data *data_a = dev_a->data;
 	const struct gpio_mspm0_config *config_a = dev_a->config;
 
-	uint32_t status_a = DL_GPIO_getRawInterruptStatus(config_a->base, 0xFFFFFFFF);
+	uint32_t status_a = DL_GPIO_getEnabledInterruptStatus(config_a->base, 0xFFFFFFFF);
 
 	DL_GPIO_clearInterruptStatus(config_a->base, status_a);
 
@@ -304,7 +304,7 @@ static void gpio_mspm0_isr(const struct device *port)
 	struct gpio_mspm0_data *data_b = dev_b->data;
 	const struct gpio_mspm0_config *config_b = dev_b->config;
 
-	uint32_t status_b = DL_GPIO_getRawInterruptStatus(config_b->base, 0xFFFFFFFF);
+	uint32_t status_b = DL_GPIO_getEnabledInterruptStatus(config_b->base, 0xFFFFFFFF);
 
 	DL_GPIO_clearInterruptStatus(config_b->base, status_b);
 

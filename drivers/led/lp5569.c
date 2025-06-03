5fd8f7382d10bd18d002792ad90828a37a4c1a9d
Tomasz Bursztyka <tobu@bang-olufsen.dk>
2025-06-03 11:17:22 +0200

    drivers: Add led current setting support on lp5569
    This controller let the possibility to set a current limit to each led,
    so let's expose this feature.
    
    Signed-off-by: Tomasz Bursztyka <tobu@bang-olufsen.dk>
    Signed-off-by: Dimitris Karnikis <dika@bang-olufsen.dk>
    Signed-off-by: Emil Dahl Juhl <emdj@bang-olufsen.dk>

--- drivers/led/lp5569.c
+++ drivers/led/lp5569.c
@@ -42,9 +42,19 @@ LOG_MODULE_REGISTER(lp5569, CONFIG_LED_LOG_LEVEL);
 #define LP5569_MASTER_FADER2     0x47
 #define LP5569_MASTER_FADER3     0x48
 
+#ifdef CONFIG_LED_CURRENT_SETTING
+/* Base register for controlling maximum delivered current */
+#define LP5569_LED0_CURRENT    0x22
+#define LP5569_MAX_CURRENT     25500
+#define LP5569_DEFAULT_CURRENT 17500
+#endif /* CONFIG_LED_CURRENT_SETTING */
+
 struct lp5569_config {
 	struct i2c_dt_spec bus;
 	struct gpio_dt_spec enable_gpio;
+#ifdef CONFIG_LED_CURRENT_SETTING
+	const uint32_t current_limit;
+#endif /* CONFIG_LED_CURRENT_SETTING */
 	const uint8_t cp_mode;
 };
 
@@ -127,9 +137,36 @@ static int lp5569_set_group_brightness(const struct device *dev, uint8_t group_i
 	return 0;
 }
 
+#ifdef CONFIG_LED_CURRENT_SETTING
+static int lp5569_set_current(const struct device *dev, uint32_t led, uint32_t micro_amps)
+{
+	const struct lp5569_config *config = dev->config;
+	uint8_t val;
+	int ret;
+
+	if (led >= LP5569_NUM_LEDS) {
+		return -EINVAL;
+	}
+
+	/* we should never exceed the device's limitations */
+	micro_amps = MIN(micro_amps, config->current_limit);
+	val = micro_amps / 100;
+
+	ret = i2c_reg_write_byte_dt(&config->bus, LP5569_LED0_CURRENT + led, val);
+	if (ret < 0) {
+		LOG_ERR("LED reg update failed");
+	}
+
+	return ret;
+}
+#endif /* CONFIG_LED_CURRENT_SETTING */
+
 static int lp5569_enable(const struct device *dev)
 {
 	const struct lp5569_config *config = dev->config;
+#ifdef CONFIG_LED_CURRENT_SETTING
+	uint8_t led_current_i2c_buf[LP5569_NUM_LEDS + 1];
+#endif
 	int ret;
 
 	if (!i2c_is_ready_dt(&config->bus)) {
@@ -169,6 +206,22 @@ static int lp5569_enable(const struct device *dev)
 		LOG_ERR("LED reg update failed");
 		return ret;
 	}
+
+#ifdef CONFIG_LED_CURRENT_SETTING
+	/*
+	 * initialize led current to the minimum between the limit configured in dts and the default
+	 * register value.
+	 */
+	led_current_i2c_buf[0] = LP5569_LED0_CURRENT;
+	memset(&led_current_i2c_buf[1], MIN(config->current_limit, LP5569_DEFAULT_CURRENT) / 100,
+	       LP5569_NUM_LEDS);
+
+	ret = i2c_write_dt(&config->bus, led_current_i2c_buf, ARRAY_SIZE(led_current_i2c_buf));
+	if (ret < 0) {
+		LOG_ERR("Failed to initialize led current");
+		return ret;
+	}
+#endif
 
 	/* apply the group brightness to MASTER_FADER1 */
 	ret = i2c_reg_write_byte_dt(&config->bus, LP5569_MASTER_FADER1, 0xFF);
@@ -241,6 +294,9 @@ static DEVICE_API(led, lp5569_led_api) = {
 	.set_brightness = lp5569_led_set_brightness,
 	.write_channels = lp5569_write_channels,
 	.set_group_brightness = lp5569_set_group_brightness,
+#ifdef CONFIG_LED_CURRENT_SETTING
+	.set_current = lp5569_set_current,
+#endif /* CONFIG_LED_CURRENT_SETTING */
 };
 
 #define LP5569_DEFINE(id)                                                                          \
@@ -248,7 +304,9 @@ static DEVICE_API(led, lp5569_led_api) = {
 		.bus = I2C_DT_SPEC_INST_GET(id),                                                   \
 		.enable_gpio = GPIO_DT_SPEC_INST_GET_OR(id, enable_gpios, {0}),                    \
 		.cp_mode = DT_ENUM_IDX(DT_DRV_INST(id), charge_pump_mode),                         \
-	};                                                                                         \
+		COND_CODE_1(IS_ENABLED(CONFIG_LED_CURRENT_SETTING),                                \
+			    (.current_limit = DT_INST_PROP_OR(id, led_max_microamp,                \
+							      LP5569_MAX_CURRENT)), ()) };         \
                                                                                                    \
 	PM_DEVICE_DT_INST_DEFINE(id, lp5569_pm_action);                                            \
                                                                                                    \

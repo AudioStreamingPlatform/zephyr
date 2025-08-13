5c6335abc037c092b56bd54dd94c11556cdd0b58
Hans Binderup <habi@bang-olufsen.dk>
2025-08-13 08:47:15 +0200

    drivers: led: lp5569: add int-clk-en config
    This commit adds the config to enable/disable internal clock.
    This is needed when running the internal engine and for clock sync
    between two units.
    
    Signed-off-by: Hans Binderup <habi@bang-olufsen.dk>

--- drivers/led/lp5569.c
+++ drivers/led/lp5569.c
@@ -30,6 +30,7 @@ LOG_MODULE_REGISTER(lp5569, CONFIG_LED_LOG_LEVEL);
 #define LP5569_CHIP_EN BIT(6)
 
 #define LP5569_MISC          0x2F
+#define LP5569_INT_CLK_EN    BIT(0)
 #define LP5569_POWERSAVE_EN  BIT(5)
 #define LP5569_EN_AUTO_INCR  BIT(6)
 #define LP5569_CP_MODE_SHIFT 3
@@ -56,6 +57,7 @@ struct lp5569_config {
 	const uint32_t current_limit;
 #endif /* CONFIG_LED_CURRENT_SETTING */
 	const uint8_t cp_mode;
+	const bool int_clk_en;
 };
 
 static int lp5569_led_set_brightness(const struct device *dev, uint32_t led, uint8_t brightness)
@@ -191,6 +193,16 @@ static int lp5569_enable(const struct device *dev)
 		k_msleep(3);
 	}
 
+	/* datasheet 8.6.1.31: only set int_clk_en if CONFIG.CHIP_EN=0 */
+	ret = i2c_reg_write_byte_dt(&config->bus, LP5569_MISC,
+				    LP5569_POWERSAVE_EN | LP5569_EN_AUTO_INCR |
+					    (config->cp_mode << LP5569_CP_MODE_SHIFT) |
+					    (config->int_clk_en ? LP5569_INT_CLK_EN : 0));
+	if (ret < 0) {
+		LOG_ERR("LED reg update failed");
+		return ret;
+	}
+
 	ret = i2c_reg_write_byte_dt(&config->bus, LP5569_CONFIG, LP5569_CHIP_EN);
 	if (ret < 0) {
 		LOG_ERR("Enable LP5569 failed");
@@ -198,14 +210,6 @@ static int lp5569_enable(const struct device *dev)
 	}
 
 	k_msleep(1);
-
-	ret = i2c_reg_write_byte_dt(&config->bus, LP5569_MISC,
-				    LP5569_POWERSAVE_EN | LP5569_EN_AUTO_INCR |
-					    (config->cp_mode << LP5569_CP_MODE_SHIFT));
-	if (ret < 0) {
-		LOG_ERR("LED reg update failed");
-		return ret;
-	}
 
 #ifdef CONFIG_LED_CURRENT_SETTING
 	/*
@@ -304,6 +308,7 @@ static DEVICE_API(led, lp5569_led_api) = {
 		.bus = I2C_DT_SPEC_INST_GET(id),                                                   \
 		.enable_gpio = GPIO_DT_SPEC_INST_GET_OR(id, enable_gpios, {0}),                    \
 		.cp_mode = DT_ENUM_IDX(DT_DRV_INST(id), charge_pump_mode),                         \
+		.int_clk_en = DT_INST_PROP_OR(id, int_clk_en, false),                              \
 		COND_CODE_1(IS_ENABLED(CONFIG_LED_CURRENT_SETTING),                                \
 			    (.current_limit = DT_INST_PROP_OR(id, led_max_microamp,                \
 							      LP5569_MAX_CURRENT)), ()) };         \

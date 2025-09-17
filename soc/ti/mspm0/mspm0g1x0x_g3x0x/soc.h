83db3d144d9160595f406d27f1a0e7fda7b4ccba
Tomasz Bursztyka <tobu@bang-olufsen.dk>
2025-09-17 12:09:44 +0200

    soc: ti: mspm0: Handle interrupt groups
    MSPM0 (G and L series so far) declare the NVIC interrupts 0 and 1
    as shared interrupts, called interrupt group 0 and 1 respectively.
    
    Such group can handle up to 8 different "interrupt" and thus require
    to be differentiated at runtime.
    
    Signed-off-by: Tomasz Bursztyka <tobu@bang-olufsen.dk>

--- soc/ti/mspm0/mspm0g1x0x_g3x0x/soc.h
+++ soc/ti/mspm0/mspm0g1x0x_g3x0x/soc.h
@@ -21,6 +21,20 @@ extern "C" {
 #define SOC_MSPM0_HFCLK_FREQ_HZ MHZ(40)
 #define SOC_MSPM0_SYSPLL_FREQ_HZ MHZ(40)
 
+/**
+ * @brief Register an isr_handler into an interrupt group
+ *
+ * @param group The targeted interrupt group (i.e. the NVIC interrupt).
+ * @param int_idx The interrupt group index (aka IIDX).
+ * @param isr_handler  Pointer to the ISR function for the device.
+ * @param dev Pointer to the device that will service the interrupt.
+ *
+ * @return 0 on success, a negative errno otherwise.
+ */
+int mspm0_register_int_to_group(int group, uint8_t int_idx,
+				void (*isr_handler)(const struct device *),
+				const struct device *dev);
+
 #ifdef __cplusplus
 }
 #endif
